10534afb24176ca85aa3e111784c2e9566002567
pm24mcgi <pmcginn@highlinefm.com>
2022-05-12 19:21:04 -0400

    login page complete

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,10 +1,7 @@
 const divClose = document.querySelector('#loginGrey')
-
-window.addEventListener('DOMContentLoaded', event => {
-    document
-        .querySelector('video')
-        .addEventListener('click', event => {
-            event.stopPropagation();
-            video.play();
-        });
+divClose.addEventListener('click', async (e) => {
+    console.log('-------IM WORKING--------')
+    await fetch('/', {
+        method: 'GET'
+    })
 });
